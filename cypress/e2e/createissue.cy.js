3209572d4e2323548fc98407d0a346a0eb511fc5
Swathi Ramamoortthy <swathi_ram@swathis-mbp.localdomain>
2023-04-27 18:04:09 +0200

    Changes made to code

--- cypress/e2e/createissue.cy.js
+++ cypress/e2e/createissue.cy.js
@@ -5,13 +5,15 @@ describe('Create JIRA issue', () => {
     cy.visit('https://jira.trungk18.com/project/board')
 
     //Click the plus icon (“Create issue”) on the left side-menu
-    cy.get(".anticon.text-xl.text-white.anticon-plus").click()
+    cy.get("app-navbar-left div:nth-child(3)").click()
     
     //Select issue type „Story”
-    cy.get("div[class='form-group'] issue-type-select").click().type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{enter}')
+    cy.get("div[class='form-group'] issue-type-select").click()
+    cy.get("nz-option-item:nth-child(2)").click()
 
     //Select issue priority „High”
-    cy.get("div[class='mt-3 form-group'] issue-priority-select").click().type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{enter}')
+    cy.get("div[class='mt-3 form-group'] issue-priority-select").click()
+    cy.get("nz-option-item:nth-child(4)").click()
     
     //Enter a short summary “Testcase for Jira Clone”
     cy.get("input[formcontrolname='title']").type("Testcase for Jira Clone")
@@ -20,8 +22,10 @@ describe('Create JIRA issue', () => {
     cy.get("div[class='ql-editor ql-blank'] p").type("As an applicant I need to create a testcase for a Jira clone")
 
     //Set the assignee to „Captain”
-    cy.get("nz-select[nzmode='multiple'] nz-select-top-control").type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{enter}')
-    
+    //cy.get("nz-select[nzmode='multiple'] nz-select-top-control").type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{upArrow}').type('{enter}')
+    cy.get("issue-assignees-select nz-select").click()
+    cy.get("nz-option-item:nth-child(3)").click()
+
     //Click „Create Issue”
     cy.get("button[type='submit'] span").click()
 
@@ -32,11 +36,14 @@ describe('Create JIRA issue', () => {
     cy.get("issue-priority j-button[class='ant-dropdown-trigger']").click()
     cy.get('span.priority-label:contains("Highest")').should('be.visible').click();
 
+     
+    // change the status to "In Progress" has to be done 
+    cy.get("issue-status j-button[class='ant-dropdown-trigger']").click()
+    cy.get("ul.mt-3>li:nth-child(2)").click()
+    
     //Close the dialog
     cy.get("j-button[icon='times'] button[type='button']").click()
 
-
-    // change the status to "In Progress" has to be done 
 
   })
 })
