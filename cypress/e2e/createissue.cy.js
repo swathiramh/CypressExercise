739b1dde9b9140f590d0241f48d02072694ef84a
Swathi Ramamoortthy <swathi_ram@swathis-mbp.localdomain>
2023-04-27 20:03:18 +0200

    object oriented design

--- cypress/e2e/createissue.cy.js
+++ cypress/e2e/createissue.cy.js
@@ -1,49 +1,78 @@
-describe('Create JIRA issue', () => {
-  it('passes', () => {
-
-    //Open the page https://jira.trungk18.com/project/board
+class JiraIssuePage {
+  visit() {
     cy.visit('https://jira.trungk18.com/project/board')
+  }
 
-    //Click the plus icon (“Create issue”) on the left side-menu
+  clickCreateIssueButton() {
     cy.get("app-navbar-left div:nth-child(3)").click()
-    
-    //Select issue type „Story”
+  }
+
+  selectIssueType(issueType) {
     cy.get("div[class='form-group'] issue-type-select").click()
-    cy.get("nz-option-item:nth-child(2)").click()
+    cy.get(`nz-option-item:nth-child(${issueType})`).click()
+  }
 
-    //Select issue priority „High”
+  selectIssuePriority(issuePriority) {
     cy.get("div[class='mt-3 form-group'] issue-priority-select").click()
-    cy.get("nz-option-item:nth-child(4)").click()
-    
-    //Enter a short summary “Testcase for Jira Clone”
-    cy.get("input[formcontrolname='title']").type("Testcase for Jira Clone")
+    cy.get(`nz-option-item:nth-child(${issuePriority})`).click()
+  }
 
-    //Enter a description „As an applicant I need to create a testcase for a Jira clone”  
-    cy.get("div[data-gramm='false']").type("As an applicant I need to create a testcase for a Jira clone")
+  enterSummary(summary) {
+    cy.get("input[formcontrolname='title']").type(summary)
+  }
 
-    //Set the assignee to „Captain”
+  enterDescription(description) {
+    cy.get("div[data-gramm='false']").type(description)
+  }
+
+  setAssignee() {
     cy.get("issue-assignees-select nz-select").click()
     cy.get("nz-option-item:nth-child(3)").click()
+  }
 
-    //Click „Create Issue”
-    cy.get("button[type='submit'] span").click()
+  clickCreateIssue() {
+    cy.get("j-button button[type='submit']").click()
+  }
 
-    //Click on the newly created issue
-    cy.get("div[id='Backlog'] issue-card:nth-child(4)").click()
+  clickOnIssue(issueNumber) {
+    cy.get(`div[id='Backlog'] issue-card:nth-child(${issueNumber})`).click()
+  }
 
-    //Change the Priority to „Highest”
-    cy.get("issue-priority j-button[class='ant-dropdown-trigger']").click()
-    cy.get('span.priority-label:contains("Highest")').should('be.visible').click();
-
-     
-    // change the status to "In Progress" has to be done 
+  changeStatusTo(status) {
     cy.get("issue-status j-button[class='ant-dropdown-trigger']").click()
-    cy.get("ul.mt-3>li:nth-child(2)").click()
-    
-    //Close the dialog
+    cy.get(`ul.mt-3>li:nth-child(${status})`).click()
+  }
+
+  changePriorityTo(priority) {
+    cy.get("issue-priority j-button[class='ant-dropdown-trigger']").click()
+    cy.get(`span.priority-label:contains(${priority})`).should('be.visible').click();
+  }
+
+  closeDialog() {
     cy.get("j-button[icon='times'] button[type='button']").click()
+  }
+}
 
+describe('Create JIRA issue', () => {
+  const jiraIssuePage = new JiraIssuePage()
 
+  beforeEach(() => {
+    jiraIssuePage.visit()
+  })
+
+  it('passes', () => {
+    jiraIssuePage.clickCreateIssueButton()
+    jiraIssuePage.selectIssueType(2)
+    jiraIssuePage.selectIssuePriority(4)
+    jiraIssuePage.enterSummary('Testcase for Jira Clone')
+    jiraIssuePage.enterDescription('As an applicant I need to create a testcase for a Jira clone')
+    jiraIssuePage.setAssignee('Captain')
+    jiraIssuePage.clickCreateIssue()
+    jiraIssuePage.clickOnIssue(4)
+    jiraIssuePage.changeStatusTo(2)
+    jiraIssuePage.changePriorityTo('Highest')
+    jiraIssuePage.closeDialog()
   })
 })
+
 
